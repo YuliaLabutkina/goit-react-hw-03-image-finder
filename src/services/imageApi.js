6a1f267549594aee8545668575d89f7fb75ac463
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-12-10 16:52:14 +0200

    added hw-3

--- src/services/imageApi.js
+++ src/services/imageApi.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const fetchImgWithQuery = async (search = '', page = 1) => {
-  const result = await axios.get(
+  const { data } = await axios.get(
     `/?q=${search}&page=${page}&key=18667081-1f708d4293c59a8f1b4f35978&image_type=photo&orientation=horizontal&per_page=12`,
   );
-  const { data } = result;
   return data.hits;
 };
 
